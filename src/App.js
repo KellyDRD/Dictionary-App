6b219a55c4efb43188505dcabe5fb36c55f1810c
KellyDRD <93556954+KellyDRD@users.noreply.github.com>
2022-06-11 13:31:17 -0500

    made a few things look nicer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="text-center">Coded by Kelly Roan</footer>
+        <footer className="text-center">
+          <small>
+            Coded by Kelly Roan and opened source on{" "}
+            <a
+              href="https://github.com/KellyDRD/dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
